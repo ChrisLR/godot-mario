6dc807144b1312154a14be151503ffad713f9839
ChrisLR <arzhul@gmail.com>
2020-10-12 12:51:11 -0400

    Tried to remove accel not very convincing

--- Entities/Mario/Mario.gd
+++ Entities/Mario/Mario.gd
@@ -2,81 +2,96 @@ extends KinematicBody2D
 
 
 var Velocity = Vector2.ZERO;
-var Accel = Vector2.ZERO
 
+var AirSpeed = 3
+var GroundSpeed = 5
 var MaxSpeed = 128
 var HalfSpeed = MaxSpeed / 2
 
+var airTime = 0
+
 var JumpHeight = -225
 var HalfJump = JumpHeight / 2
-var Gravity = Vector2(0, 0.5)
+var Gravity = Vector2(0, 6)
 var Jumping = false;
 
 var coins: int = 0;
+var isSmall: = true
 
 onready var groundCast = $GroundCast
 onready var startPoint = position
+onready var CurrentSprite = $Small
+
+var grounded = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
-func on_change_direction(isGrounded):
-	if Accel.x > 0:
-		$Small.flip_h = false
-	elif Accel.x < 0:
-		$Small.flip_h = true
 	
-	Accel.x = 0
-	if isGrounded:
-		$Small.play("Slide")
 
+func move_left():
+	CurrentSprite.flip_h = true
+	if Velocity.x > 0 and grounded:
+		CurrentSprite.play("Slide")
+	elif grounded:
+		CurrentSprite.play("Move")
+		
+	if not grounded:
+		Velocity.x -= AirSpeed
+	else:
+		Velocity.x -= GroundSpeed
+
+func move_right():
+	CurrentSprite.flip_h = false
+	if Velocity.x < 0 and grounded:
+		CurrentSprite.play("Slide")
+	elif grounded:
+		CurrentSprite.play("Move")
+		
+	if not grounded:
+		Velocity.x += AirSpeed
+	else:
+		Velocity.x += GroundSpeed
 
 func _physics_process(_delta):
-	var isGrounded = groundCast.is_colliding() or is_on_floor();
-	if Input.is_action_pressed("ui_right"):
-		if Velocity.x < 0:
-			on_change_direction(isGrounded)
-		else:
-			if isGrounded:
-				$Small.play("Move")
-		Accel.x += 1;
-	elif Input.is_action_pressed("ui_left"):
-		if Velocity.x > 0:
-			if isGrounded:
-				on_change_direction(isGrounded)
-		else:
-			if isGrounded:
-				$Small.play("Move")
-		Accel.x -= 1;
-	else:
-		Accel.x = 0;
+	Velocity = move_and_slide(Velocity, Vector2.UP);
+	grounded = groundCast.is_colliding() or is_on_floor();
 	
-	if Input.is_action_pressed("ui_up") and isGrounded:
+	if Input.is_action_pressed("ui_right"):
+		move_right()
+	elif Input.is_action_pressed("ui_left"):
+		move_left()
+	else:
+		# Not Moving
+		if Velocity.x < 0:
+			Velocity.x += 2
+		elif Velocity.x > 0:
+			Velocity.x -= 2
+	
+	if Input.is_action_pressed("ui_up") and grounded:
 		Velocity.y = JumpHeight;
 		$Small.play("Jump")
 		Jumping = true
-	elif isGrounded and Jumping:
-		if Accel.x != 0:
-			$Small.play("Move")
-		else:
+	elif grounded:
+		airTime = 0
+		if Velocity.x > -0.5 and Velocity.x < 0.2:
 			$Small.play("Idle")
 	
-	if not isGrounded:
-		Accel.y += Gravity.y
+	if not grounded or Jumping:
+		airTime += 1
+		Velocity += Gravity
+		Velocity.y += int(airTime / 2)
 	else:
-		Accel.y = 0
-	
-	Velocity += Accel;
+		if Velocity.x < 0:
+			Velocity.x += 1
+		elif Velocity.x > 0:
+			Velocity.x -= 1
+			
+		Velocity.y = 1
 	
 	Velocity.x = clamp(Velocity.x, -MaxSpeed, MaxSpeed)
-
-	Accel.x = clamp(Accel.x, -HalfSpeed, HalfSpeed)
-	Velocity = move_and_slide(Velocity, Vector2.UP);
 	
-	Velocity.x = lerp(Velocity.x, 0, 0.3)
 
 
 func add_coin(value):
