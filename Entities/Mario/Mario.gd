e6b773a7d51c014ad85e532d3425f7cdd85c4cdd
ChrisLR <arzhul@gmail.com>
2020-10-12 11:57:31 -0400

    Tried to improve Mario's handling, not very successful

--- Entities/Mario/Mario.gd
+++ Entities/Mario/Mario.gd
@@ -3,41 +3,50 @@ extends KinematicBody2D
 
 var Velocity = Vector2.ZERO;
 var Accel = Vector2.ZERO
+
 var MaxSpeed = 128
-var Gravity = Vector2(0, 1)
+var HalfSpeed = MaxSpeed / 2
+
+var JumpHeight = -225
+var HalfJump = JumpHeight / 2
+var Gravity = Vector2(0, 0.5)
 var Jumping = false;
 
 var coins: int = 0;
 
 onready var groundCast = $GroundCast
+onready var startPoint = position
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-	
-func _process(delta):
-	pass
-		
 
-func _physics_process(delta):
+
+func on_change_direction(isGrounded):
+	if Accel.x > 0:
+		$Small.flip_h = false
+	elif Accel.x < 0:
+		$Small.flip_h = true
+	
+	Accel.x = 0
+	if isGrounded:
+		$Small.play("Slide")
+
+
+func _physics_process(_delta):
 	var isGrounded = groundCast.is_colliding() or is_on_floor();
 	if Input.is_action_pressed("ui_right"):
-		$Small.flip_h = false
-		if Accel.x < 0:
-			if isGrounded:
-				$Small.play("Slide")
-			Accel.x += 2;
+		if Velocity.x < 0:
+			on_change_direction(isGrounded)
 		else:
 			if isGrounded:
 				$Small.play("Move")
 		Accel.x += 1;
 	elif Input.is_action_pressed("ui_left"):
-		$Small.flip_h = true
-		if Accel.x > 0:
+		if Velocity.x > 0:
 			if isGrounded:
-				$Small.play("Slide")
-			Accel.x -= 2;
+				on_change_direction(isGrounded)
 		else:
 			if isGrounded:
 				$Small.play("Move")
@@ -45,13 +54,8 @@ func _physics_process(delta):
 	else:
 		Accel.x = 0;
 	
-	if Jumping:
-		var collider = $CeilingCast.get_collider()
-		if collider and collider.has_method("on_bump"):
-			collider.on_bump(self)
-	
 	if Input.is_action_pressed("ui_up") and isGrounded:
-		Velocity.y = -256;
+		Velocity.y = JumpHeight;
 		$Small.play("Jump")
 		Jumping = true
 	elif isGrounded and Jumping:
@@ -61,18 +65,43 @@ func _physics_process(delta):
 			$Small.play("Idle")
 	
 	if not isGrounded:
-		Accel.y += 1
+		Accel.y += Gravity.y
 	else:
 		Accel.y = 0
 	
 	Velocity += Accel;
 	
 	Velocity.x = clamp(Velocity.x, -MaxSpeed, MaxSpeed)
-	Accel.x = clamp(Accel.x, -MaxSpeed, MaxSpeed)
+
+	Accel.x = clamp(Accel.x, -HalfSpeed, HalfSpeed)
 	Velocity = move_and_slide(Velocity, Vector2.UP);
 	
-	Velocity.x = lerp(Velocity.x, 0, 0.2)
-	
+	Velocity.x = lerp(Velocity.x, 0, 0.3)
+
 
 func add_coin(value):
 	coins += value
+
+func on_damage():
+	pass
+	
+func respawn():
+	position = startPoint
+
+
+func _on_AreaHead_body_entered(body):
+	if Jumping and body.has_method("on_bump"):
+		body.on_bump(self)
+
+
+func _on_AreaFeet_body_entered(body):
+	if body.has_method("on_crush"):
+		body.on_crush(self)
+	Jumping = true
+	Velocity.y = HalfJump;
+	$Small.play("Jump")
+	
+func _on_AreaBody_body_entered(body):
+	if body.has_method("on_attack"):
+		body.on_attack(self)
+		self.on_damage()
