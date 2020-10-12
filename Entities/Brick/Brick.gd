0125e82f1f66ecb5c2265d07b98116458e994bc5
ChrisLR <arzhul@gmail.com>
2020-10-12 12:50:29 -0400

    Adjustments

--- Entities/Brick/Brick.gd
+++ Entities/Brick/Brick.gd
@@ -1,11 +1,14 @@
 extends StaticBody2D
 
-func on_bump(_bumper):
-	$AnimationPlayer.play("Break")
-	$FragmentBL.visible = true
-	$FragmentBR.visible = true
-	$FragmentTL.visible = true
-	$FragmentTR.visible = true
+func on_bump(bumper):
+	if not bumper.isSmall:
+		$AnimationPlayer.play("Break")
+		$FragmentBL.visible = true
+		$FragmentBR.visible = true
+		$FragmentTL.visible = true
+		$FragmentTR.visible = true
+	else:
+		$AnimationPlayer.play("Hit")
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
